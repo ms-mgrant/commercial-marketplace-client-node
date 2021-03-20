3144cdd740c5d789ab8a8e4fa2334b2a09193937
Scott Seely <scseely@microsoft.com>
2021-03-20 16:07:55 -0700

    Basic authentication works, lists subscriptions.

--- saas/test/saaSAPI.spec.ts
+++ saas/test/saaSAPI.spec.ts
@@ -10,6 +10,38 @@ import { expect } from 'chai';
 import * as coreHttp from "@azure/core-http";
 import { SaaSAPI } from '../src/saaSAPI';
 import * as azureIdentity from "@azure/identity";
+import {WebResourceLike} from "@azure/core-http/types/3.1/src/webResource";
+import {HttpHeaders} from "@azure/core-http";
+
+class MarketplaceCredentials {
+    constructor(tokenCredential : azureIdentity.TokenCredential) {
+        this.tokenCredential = tokenCredential;
+    }
+
+    public async signRequest(webResource: WebResourceLike): Promise<WebResourceLike>{
+
+        if (!webResource) {
+            return Promise.reject(new Error("webResource cannot be null or undefined and must be of type \"object\"."));
+        }
+
+        if (!webResource.headers) {
+            webResource.headers = new HttpHeaders();
+        }
+
+        let token = await this.tokenCredential.getToken( "20e940b3-4c77-4b0b-9a53-9e16a1b010a7/.default");
+
+        if (token) {
+            webResource.headers.set("Authorization", "Bearer " + token.token);
+        } else {
+            return Promise.reject(new Error("Could not authorize the token."));
+        }
+
+        return Promise.resolve(webResource);
+    }
+
+    tokenCredential: azureIdentity.TokenCredential;
+}
+
 
 function get_client_secret_credential() {
     let tenantId = process.env.AAD_TENANT_ID || '';
@@ -36,11 +68,11 @@ function get_client_secret_credential() {
         clientId!,
         clientSecret!);
 
-    return credential;
+    return new MarketplaceCredentials(credential);
 }
 
 describe ("SaaSAPI", () => {
-    describe("CredentialAuth", () => {
+    describe("Basics", () => {
         it ("Should get a list of subscriptions", async () => {
             let creds = get_client_secret_credential();
             let saasClient = new SaaSAPI(creds);
@@ -49,6 +81,7 @@ describe ("SaaSAPI", () => {
             for (let iter = await subs.next(); iter != undefined && iter != null; iter = await subs.next()) {
                 foundAtLeastOne = true;
                 console.log(iter.value.id);
+                break;
             }
             expect(foundAtLeastOne).to.be.true;
         })
